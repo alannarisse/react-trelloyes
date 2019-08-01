934aa3ea3b6a764843c88bdb75a020dbf9e33387
alannarisse <angrypirate@gmail.com>
2019-08-01 12:14:09 -0700

    added package.json changes

---
+++ build/precache-manifest.f294d9ec6e833f9953d38d3073cb8dd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5f67d264acac5a934a352092b70750",
+    "url": "/react-trelloyes/index.html"
+  },
+  {
+    "revision": "4bc27f2437e11537c9cc",
+    "url": "/react-trelloyes/static/css/main.984ace5c.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-trelloyes/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "4bc27f2437e11537c9cc",
+    "url": "/react-trelloyes/static/js/main.c07caf98.chunk.js"
+  },
+  {
+    "revision": "658aba3b58d8e166956e",
+    "url": "/react-trelloyes/static/js/runtime~main.bc44e323.js"
+  }
+]);
