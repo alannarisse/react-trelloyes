fbb873412c40ca7d140a5831f5413915d729383a
alannarisse <angrypirate@gmail.com>
2019-08-01 12:33:36 -0700

    Updates

---
+++ precache-manifest.c6db24513be1e37583dbaffdd4d0f0e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a218b5aba65558b9c54cfdd64a18f91",
+    "url": "/react-trelloyes/index.html"
+  },
+  {
+    "revision": "14c8a0768813bf21b465",
+    "url": "/react-trelloyes/static/css/main.984ace5c.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-trelloyes/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "14c8a0768813bf21b465",
+    "url": "/react-trelloyes/static/js/main.acdc8241.chunk.js"
+  },
+  {
+    "revision": "658aba3b58d8e166956e",
+    "url": "/react-trelloyes/static/js/runtime~main.bc44e323.js"
+  }
+]);
